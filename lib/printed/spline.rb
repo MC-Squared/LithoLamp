549d51246e6a7370eee6db107fd6bd5cac1941fa
J Miles <j.milesy.nz@gmail.com>
2017-09-10 20:32:52 +1200

    Separate ridge_height from spline_width

--- lib/printed/spline.rb
+++ lib/printed/spline.rb
@@ -5,23 +5,23 @@ class Spline < SolidRuby::Printed
 
     lp = Params::LAMP_PARAMS
     @steps = steps
-    @ridge_h = lp[:ridge_height]
+    @spline_w = lp[:spline_width]
     @step_size = lp[:step_size]
     @tolerance = add_tolerance ? lp[:tolerance] : 0
   end
 
   def part(_show)
-    res = cube(x: @ridge_h*10.0, y: @ridge_h + @tolerance, z: @step_size * @steps)
+    res = cube(x: @spline_w*10.0, y: @spline_w + @tolerance, z: @step_size * @steps)
       .center_xy +
-    cylinder(d: @ridge_h*1.5 + @tolerance, h: @step_size * @steps)
-       .translate(x: -@ridge_h*5.0, y: -@ridge_h/4.0) +
-     cylinder(d: @ridge_h*1.5 + @tolerance, h: @step_size * @steps)
-       .translate(x: @ridge_h*5.0, y: -@ridge_h/4.0)
+    cylinder(d: @spline_w*1.5 + @tolerance, h: @step_size * @steps)
+       .translate(x: -@spline_w*5.0, y: -@spline_w/4.0) +
+     cylinder(d: @spline_w*1.5 + @tolerance, h: @step_size * @steps)
+       .translate(x: @spline_w*5.0, y: -@spline_w/4.0)
 
     # lay flat for output
     unless (_show)
       res.rotate(x: -90)
-        .translate(z: @ridge_h/2.0)
+        .translate(z: @spline_w/2.0)
     end
 
     res
